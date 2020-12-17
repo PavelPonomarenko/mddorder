6d2ca951bb843f701970bd1256955e139dc78a3a
PavelPonomarenko <ponomarenko.kyiv@gmail.com>
2020-12-17 12:29:10 +0300

    Feature: add users in Spring Security

--- src/main/java/ua/com/mddorder/config/WebSecurityConfig.java
+++ src/main/java/ua/com/mddorder/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ua.com.mddorder.domain.Role;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/authenticated/**","/**").authenticated()
+                .antMatchers("/authenticated/**", "/**").authenticated()
 //                .antMatchers("/admin/**").hasAnyRole("ADMIN")
 //                .antMatchers("/profile/**").authenticated()
                 .and()
@@ -26,16 +27,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutSuccessUrl("/");
     }
 
-//    @Bean
+    @Bean
 //    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("111")
+                .roles(String.valueOf(Role.USER))
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("222")
+                .roles(String.valueOf(Role.ADMIN))
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
